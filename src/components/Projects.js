3af19d4a69566518b1daf9622f08337dc5fcf5e2
codersurya0904 <89637956+surya0904shankar@users.noreply.github.com>
2025-04-13 16:44:44 +0530

    Add files via upload

---
+++ src/components/Projects.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <div className="container">
+      <h1>My Projects</h1>
+      <div className="projects-grid">
+        {/* Add your projects here */}
+      </div>
+    </div>
+  );
+}
+
+export default Projects; 
