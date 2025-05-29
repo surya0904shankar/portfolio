93410e26f00969cf2ec24197713d3a4d9b0cedda
surya0904shankar <suryas0904@gmail.com>
2025-05-29 19:53:23 +0530

    Update profile photo path to use correct image file

--- src/components/Home.js
+++ src/components/Home.js
@@ -32,7 +32,7 @@ function Home() {
           </div>
           <div className="profile-image-container">
             <img 
-              src={`${process.env.PUBLIC_URL}/assets/profile-photo.jpg`}
+              src={`${process.env.PUBLIC_URL}/assets/WhatsApp Image 2025-04-13 at 12.06.45 PM.jpeg`}
               alt="Profile Photo" 
               className="profile-image"
             />
