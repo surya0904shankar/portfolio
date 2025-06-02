2f557cde9416513c3646f8c7217730eda1bba662
surya0904shankar <suryas0904@gmail.com>
2025-06-02 09:51:29 +0530

    fix: resolve profile image visibility issue and merge conflict

--- src/components/Home.js
+++ src/components/Home.js
@@ -33,7 +33,7 @@ function Home() {
           </div>
           <div className="profile-image-container">
             <img 
-              src={profilePhoto}
+              src={`${process.env.PUBLIC_URL}/assets/certificates/WhatsApp Image 2025-04-12 at 10.02.16 PM.jpeg`}
               alt="Profile Photo" 
               className="profile-image"
             />
